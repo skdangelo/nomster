7779bdb1484edb5a0f56d822ff560ddb806417e3
Sandra D'Angelo <skdangelo@gmail.com>
2019-04-30 01:10:06 +0000

    Add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
+//= require jquery
+//= rquire jquery_ujs
 //= require turbolinks
+//= require popper
+//= require tether
+//= require bootstrap-sprockets
 //= require_tree .
