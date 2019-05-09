5fb272dac2c1e3d1526fc9d0bbd57297716c4274
Sandra D'Angelo <skdangelo@gmail.com>
2019-05-09 00:34:12 +0000

    Setup Places subform and connected to database

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,21 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.all
+    @places = Place.all.page(params[:page]).per(5)
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+     Place.create(place_params)
+     redirect_to root_path
+  end
+
+  private
+
+  def place_params
+     params.require(:place).permit(:name, :description, :address)
   end
 end
