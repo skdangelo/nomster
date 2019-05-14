c836c67052996f5699269d4e15903fd2bea9109b
Sandra D'Angelo <skdangelo@gmail.com>
2019-05-14 20:32:00 +0000

    Added map to place page

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,9 @@
 class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :address
-  after_validation :geocoded_by
-  
-  validates :name, length: { is: 3 }, presence: true
+  after_validation :geocode
+
+  validates :name, length: { minimum: 3 }, presence: true
   validates :address, presence: true
   validates :description, presence: true
 end
